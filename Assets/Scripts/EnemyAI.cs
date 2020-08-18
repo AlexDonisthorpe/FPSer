a5212ca43c0f18d68304f587c0afba01652a3caf
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-08-18 14:32:00 +0100

    Refactored enemy engagement script, added a placeholder weapon to the player

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,6 +9,7 @@ public class EnemyAI : MonoBehaviour
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 5f;
 
+    bool isProvoked = false;
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
 
@@ -21,15 +23,42 @@ public class EnemyAI : MonoBehaviour
     void Update()
     {
         distanceToTarget = Vector3.Distance(target.position, transform.position);
-        if (distanceToTarget < chaseRange)
+
+        if (isProvoked)
         {
-            navMeshAgent.SetDestination(target.position);
+            EngageTarget();
+        } else if (distanceToTarget < chaseRange)
+        {
+            isProvoked = true;
         }
+    }
+
+    private void EngageTarget()
+    {
+        if (distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        else if(distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            Attack();
+        }
+
+    }
+
+    private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private static void Attack()
+    {
+        print("gotcha!");
     }
 
     private void OnDrawGizmosSelected()
     {
-        Gizmos.color = new Color(1, 0, 0, 0.5f);
-        Gizmos.DrawSphere(transform.position, chaseRange);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
 }
