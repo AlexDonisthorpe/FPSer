0f3a9a217fa22442ba8cdcfa939117f95d608414
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-08-24 13:43:10 +0100

    Added muzzle flash VFX on shoot

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     [SerializeField] Camera FPCamera;
+    [SerializeField] ParticleSystem muzzleFlash;
     [SerializeField] float range = 100f;
     [SerializeField] int damage = 10;
 
@@ -26,18 +27,30 @@ public class Weapon : MonoBehaviour
 
     private void Shoot()
     {
+        PlayMuzzleFlash();
+        ProcessRaycast();
+
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void ProcessRaycast()
+    {
         RaycastHit hit;
-        if(Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
             Debug.Log(hit.transform.name + " hit!");
             // TODO: add some hit effect for visual feedback
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) return;
             target.TakeDamage(damage);
         }
         else
         {
             return;
         }
-
     }
 }
