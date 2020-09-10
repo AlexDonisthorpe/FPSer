04a7a4901cd65637a15440d300f614b66fd153e1
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-09-10 17:10:55 +0100

    Added a game over screen and some basic scene management

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    Scene currentScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(currentScene.buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
