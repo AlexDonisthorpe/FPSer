f34d2fcd97aeb5c4b0587e8642f73ff5316f9403
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-09-11 16:56:08 +0100

    Enabled weapon switching via scrollwheel & number key select, additionally disabled weapon switching on death.

--- Assets/Scripts/WeaponSwitcher.cs
+++ Assets/Scripts/WeaponSwitcher.cs
@@ -34,6 +34,52 @@ public class WeaponSwitcher : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        int previousWeapon = currentWeapon;
+        ProcessKeyInput();
+        ProcessScrollWheel();
+
+        if(previousWeapon != currentWeapon)
+        {
+            SetWeaponActive();
+        }
+    }
+
+    private void ProcessScrollWheel()
+    {
+        if(Input.GetAxis("Mouse ScrollWheel") > 0)
+        {
+            if(currentWeapon >= transform.childCount - 1)
+            {
+                currentWeapon = 0;
+            }
+            else
+            {
+                currentWeapon++;
+            }
+        } else if(Input.GetAxis("Mouse ScrollWheel") < 0)
+        {
+            if(currentWeapon <= 0)
+            {
+                currentWeapon = 2;
+            }
+            else
+            {
+                currentWeapon--;
+            }
+        }
+    }
+
+    private void ProcessKeyInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            currentWeapon = 0;
+        } else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            currentWeapon = 1;
+        } else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            currentWeapon = 2;
+        }
     }
 }
