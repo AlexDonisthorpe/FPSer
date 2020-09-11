5a32b57488259689d5ca5d73e9e25ef5e0ddb05a
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-09-11 01:07:10 +0100

    Implemented weapon zoom by tweeking FOV on button press.

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+    Camera playerCamera;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+    // Start is called before the first frame update
+
+    bool zoomedInToggle = false;
+
+    void Start()
+    {
+        playerCamera = FindObjectOfType<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+         playerCamera.fieldOfView = zoomedInFOV;
+        }
+        else
+        {
+            playerCamera.fieldOfView = zoomedOutFOV;
+        }
+    }
+}
