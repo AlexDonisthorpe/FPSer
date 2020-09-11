31e33b70253c51c764bb061bdf3e83ce6d1d8b75
Alex Donisthorpe <alex.donisthorpe+doneecans@gmail.com>
2020-09-11 21:01:11 +0100

    Created Ammo pickups for the variety of weapons

---
+++ Assets/Scripts/AmmoPickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoPickup : MonoBehaviour
+{
+    [SerializeField] int ammoAmount = 1;
+    [SerializeField] AmmoType ammoType = default;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<Ammo>().IncreaseCurrentAmmo(ammoType, ammoAmount);
+            Destroy(gameObject, 1f);
+        }
+    }
+}
